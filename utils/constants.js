a28688404994819230886cb4d732c084b3494a0b
kmkcodelab <kmkcodelab@gmail.com>
2026-02-06 22:10:55 +0600

    Create constants.js

---
+++ utils/constants.js
@@ -0,0 +1,23 @@
+export const DEFAULT_HOME = 'https://www.google.com';
+
+export const SEARCH_ENGINES = {
+  google: 'https://www.google.com/search?q=',
+  bing: 'https://www.bing.com/search?q=',
+  duckduckgo: 'https://duckduckgo.com/?q=',
+};
+
+export const MAX_TABS = 50;
+export const MAX_HISTORY = 200;
+
+export const COLORS = {
+  bg: '#1a1a2e',
+  surface: '#16213e',
+  surfaceLight: '#1c2a4a',
+  accent: '#e94560',
+  text: '#ffffff',
+  textSecondary: '#a0a0b0',
+  textMuted: '#666680',
+  border: '#2a2a4a',
+  success: '#4ecca3',
+  danger: '#e94560',
+};
